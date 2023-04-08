b0d7507610a6254eda80c96a1e9adb0ce479d91f
Ram Krishna Singh <rkscodes@gmail.com>
2023-04-08 11:41:12 +0000

    :wrench: Added files to ignore
    Added more file to ignore

--- prefect/blocks/test.py
+++ prefect/blocks/test.py
@@ -1,25 +1,23 @@
-#!/usr/bin/env python
+from pathlib import Path
+from prefect_gcp.cloud_storage import GcsBucket
 
-# make sure to install these packages before running:
-# pip install pandas
-# pip install sodapy
 
-import pandas as pd
-from sodapy import Socrata
 
-# Unauthenticated client only works with public data sets. Note 'None'
-# in place of application token, and no username or password:
-client = Socrata("data.sfgov.org", None)
+from_path = Path('../../data/2023/03/06.csv').resolve()
+to_path = Path('data/2023/03/06.csv')
 
-# Example authenticated client (needed for non-public datasets):
-# client = Socrata(data.sfgov.org,
-#                  MyAppToken,
-#                  username="user@example.com",
-#                  password="AFakePassword")
+gcs_block =  GcsBucket.load("gcp-bucket-block")
+gcs_block.upload_from_path(from_path=from_path, to_path=to_path)
 
-# First 2000 results, returned as JSON from API / converted to Python list of
-# dictionaries by sodapy.
-results = client.get("wg3w-h783", limit=2000)
+# gcs_block.download_folder_to_path(from_folder="data/2023/03/", to_folder="data")
+# gcs_block.download_object_to_path(from_path='data/2023/03/27.csv' ,to_path= 'abc.csv')
 
-# Convert to pandas DataFrame
-results_df = pd.DataFrame.from_records(results)
+
+# blob_list = gcs_block.list_blobs()
+
+# file_paths = [blob.name for blob in blob_list]
+
+# if str(to_path) in file_paths: 
+#     print("true that")
+
+# print(file_paths)
