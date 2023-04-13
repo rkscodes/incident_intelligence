9888a74ec3ebb81d1b5db8fd4b5617a810c23ea4
Ram Krishna Singh <rkscodes@gmail.com>
2023-04-14 01:27:22 +0530

    :art: Made consistent by returing Path
    Made consistent by returing Path obj instead of string

--- pipeline/flows/utils/renamed_file_name.py
+++ pipeline/flows/utils/renamed_file_name.py
@@ -1,6 +1,9 @@
-def renamed_file_name(path: str):
-    # file_name = str(path)
-    parts = path.split("/")
+from pathlib import Path
+
+
+def renamed_file_name(path: Path):
+    file_name = str(path)
+    parts = file_name.split("/")
     just_name = parts[-1].split(".")[-2]
     new_name = parts[-4] + "/" + parts[-3] + "/" + parts[-2] + "/" + just_name + ".csv"
-    return new_name
+    return Path(new_name)
