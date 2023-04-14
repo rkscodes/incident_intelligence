4157f2e27a62fb5037869e5c67833a711b9733b2
Ram Krishna Singh <rkscodes@gmail.com>
2023-04-14 10:16:58 +0530

    :recycle: Refactored load_gcs_flow
    Added seprate function for to_path and improved overall style

--- pipeline/flows/load_gcs_flow.py
+++ pipeline/flows/load_gcs_flow.py
@@ -1,14 +1,32 @@
-from prefect import flow
-from prefect_gcp.cloud_storage import GcsBucket
+from datetime import datetime
+from pathlib import Path
 from typing import List
+
+from prefect import flow, task
+from prefect_gcp.cloud_storage import GcsBucket
 from utils.renamed_file_name import renamed_file_name
 
 
 @flow(log_prints=True)
-def load_gcs(files: List):
+def load_gcs(files: List) -> Path:
     gcs_block = GcsBucket.load("gcp-bucket-block")
+    gcs_file_path = []
 
     for path in files:
-        from_path = path
-        to_path = renamed_file_name(path)
-        gcs_block.upload_from_path(from_path=from_path, to_path=to_path)
+        upload_path = to_path(path=path)
+        gcs_block.upload_from_path(from_path=path, to_path=upload_path)
+        gcs_file_path.append(to_path(path=upload_path))
+
+    return gcs_file_path
+
+
+@task()
+def to_path(path: Path) -> Path:
+    today = datetime.today()
+    year, month, day = (
+        str(today.year),
+        str(today.month).zfill(2),
+        str(today.day).zfill(2),
+    )
+    path = Path(year) / Path(month) / Path(day) / renamed_file_name(path)
+    return path
