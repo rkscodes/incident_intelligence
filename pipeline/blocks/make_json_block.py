24e4548d1b65236198e923532342a38ffebbe8d7
Ram Krishna Singh <rkscodes@gmail.com>
2023-04-16 09:48:48 +0530

    :sparkles: Added json block from config.json
    It removes the need of constant read from config.json, and just will allow to read from this block

---
+++ pipeline/blocks/make_json_block.py
@@ -0,0 +1,19 @@
+import json
+
+from prefect.blocks.system import JSON
+
+with open("config.json", "r") as config_file:
+    config = json.load(config_file)
+
+# Set the value of the JSON block
+json_block_value = {
+    "project_id": config.get("project_id"),
+    "dataset_id": config.get("dataset_id"),
+    "table_id": config.get("dataset_id"),
+    "service_account_file_path": config.get("service_account_file_path"),
+    "gcs_bucket_name": config.get("gcs_bucket_name"),
+}
+
+# Create the JSON block
+json_block = JSON(value=json_block_value)
+json_block.save("json-config")
