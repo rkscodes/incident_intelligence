9e30d5a737a22af785fbc5f9a3f6bdb303b8c333
Ram Krishna Singh <rkscodes@gmail.com>
2023-04-17 23:43:54 +0530

    :zap: Stopped downloading git objects
    That file is not necessary so stopped downloading it in first place

--- pipeline/blocks/make_github_repo_block.py
+++ pipeline/blocks/make_github_repo_block.py
@@ -2,5 +2,9 @@ from prefect.blocks.system import JSON
 from prefect.filesystems import GitHub
 
 json_block = JSON.load("json-config")
-block = GitHub(repository=json_block.value["github_repo"], reference=json_block.value["branch"])
+block = GitHub(
+    repository=json_block.value["github_repo"],
+    reference=json_block.value["branch"],
+    include_git_objects=False,
+)
 block.save("project-code")
