b3ae99af1d7b44fce3b0c0707b36e1c5c6d2a908
Ram Krishna Singh <rkscodes@gmail.com>
2023-04-17 23:52:06 +0530

    :sparkles: Deployment script in python
    Using python to deploy rather than yaml files

---
+++ pipeline/flows/infra-docker-storage-github_deployment.py
@@ -0,0 +1,21 @@
+from parent_etl_flow import parent_etl_flow
+from prefect.deployments import Deployment
+from prefect.filesystems import GitHub
+from prefect.infrastructure import DockerContainer
+
+github_block = GitHub.load("project-code")
+
+
+docker_container_block = DockerContainer.load("docker-block")
+
+deployment = Deployment.build_from_flow(
+    flow=parent_etl_flow,
+    name="infra-docker-storage-github",
+    infrastructure=docker_container_block,
+    storage=github_block,
+    path="",
+    entrypoint="pipeline/flows/parent_etl_flow.py:parent_etl_flow",
+    ignore_file=".prefectignore",
+    skip_upload=True,
+)
+deployment.apply()
