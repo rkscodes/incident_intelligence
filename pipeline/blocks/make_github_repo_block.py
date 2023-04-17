df421c39823db1b5aa4fd9e687ad74ab1f4a3291
Ram Krishna Singh <rkscodes@gmail.com>
2023-04-17 17:10:14 +0530

    :art: Formated using black
    Just a simple formating

--- pipeline/blocks/make_github_repo_block.py
+++ pipeline/blocks/make_github_repo_block.py
@@ -1,8 +1,6 @@
-from prefect.filesystems import GitHub
 from prefect.blocks.system import JSON
+from prefect.filesystems import GitHub
 
 json_block = JSON.load("json-config")
-block = GitHub(
-    repository=json_block.value["github_repo"], reference=json_block.value["branch"]
-)
+block = GitHub(repository=json_block.value["github_repo"], reference=json_block.value["branch"])
 block.save("project-code")
