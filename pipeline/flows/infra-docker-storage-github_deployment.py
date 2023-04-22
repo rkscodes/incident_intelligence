676cb06416d45371525de49f8a5ca7916848aa03
Ram Krishna Singh <rkscodes@gmail.com>
2023-04-22 19:55:43 +0530

    :sparkles: Added cron schedule
    to run on midnight in IST timezone

--- pipeline/flows/infra-docker-storage-github_deployment.py
+++ pipeline/flows/infra-docker-storage-github_deployment.py
@@ -2,10 +2,9 @@ from parent_etl_flow import parent_etl_flow
 from prefect.deployments import Deployment
 from prefect.filesystems import GitHub
 from prefect.infrastructure import DockerContainer
+from prefect.server.schemas.schedules import CronSchedule
 
 github_block = GitHub.load("project-code")
-
-
 docker_container_block = DockerContainer.load("docker-block")
 
 deployment = Deployment.build_from_flow(
@@ -17,5 +16,7 @@ deployment = Deployment.build_from_flow(
     entrypoint="pipeline/flows/parent_etl_flow.py:parent_etl_flow",
     ignore_file=".prefectignore",
     skip_upload=True,
+    schedule=CronSchedule(cron="0 0 * * *", timezone="Asia/Calcutta"),
+    is_schedule_active = False,
 )
 deployment.apply()
