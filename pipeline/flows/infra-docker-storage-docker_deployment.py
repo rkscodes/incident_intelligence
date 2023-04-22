4843d466185d796d17b22b5ed20835fec1f49b14
Ram Krishna Singh <rkscodes@gmail.com>
2023-04-22 08:51:59 +0530

    :truck: Renamed infra-docker-storage-local
    to infra-docker-storage-docker

---
+++ pipeline/flows/infra-docker-storage-docker_deployment.py
@@ -0,0 +1,16 @@
+from parent_etl_flow import parent_etl_flow
+from prefect.deployments import Deployment
+from prefect.infrastructure import DockerContainer
+
+docker_container_block = DockerContainer.load("docker-block")
+
+deployment = Deployment.build_from_flow(
+    flow=parent_etl_flow,
+    name="infra-docker-storage-local",
+    infrastructure=docker_container_block,
+    path="/",
+    entrypoint="pipeline/flows/parent_etl_flow.py:parent_etl_flow",
+    ignore_file=".prefectignore",
+    skip_upload=True,
+)
+deployment.apply()
