d9aa5b87567e8252b43ec0099242f7ed2183c7d8
Ram Krishna Singh <rkscodes@gmail.com>
2023-04-26 09:38:16 +0530

    :building_construction: Added a always free tier compute instance
    So that I can keep this project live for quite a while

--- terraform/main.tf
+++ terraform/main.tf
@@ -47,51 +47,110 @@ resource "google_compute_firewall" "allow443" {
 }
 
 
-resource "google_compute_instance" "deincidentcompute" {
-  name         = "deincidentcompute"
-  machine_type = "e2-custom-4-9216"
+# resource "google_compute_instance" "deincidentcompute" {
+#   name         = "deincidentcompute"
+#   machine_type = "e2-custom-4-9216"
+#   zone         = var.zone
+
+#   boot_disk {
+#     auto_delete = true
+#     initialize_params {
+#       image = "ubuntu-os-cloud/ubuntu-2004-focal-v20230302"
+#       size  = "80"
+#       type  = "pd-balanced"
+#     }
+#     mode = "READ_WRITE"
+#   }
+
+#   network_interface {
+#     network = "default"
+#     # subnetwork = "regions/us-west1/subnetworks/default"
+#     # subnetwork = "regions/${var.region}/subnetworks/default"
+#     subnetwork = "projects/${var.project}/regions/${var.region}/subnetworks/default"
+
+#     access_config {
+#       #   name         = "External NAT"
+#       network_tier = "PREMIUM"
+#     }
+#   }
+
+#   can_ip_forward      = false
+#   deletion_protection = false
+#   enable_display      = false
+
+#   allow_stopping_for_update = true
+
+
+#   tags = ["http-server", "https-server"]
+
+#   scheduling {
+#     automatic_restart   = true
+#     on_host_maintenance = "MIGRATE"
+#     provisioning_model  = "STANDARD"
+#     preemptible         = false
+#   }
+
+#   shielded_instance_config {
+#     enable_integrity_monitoring = true
+#     enable_secure_boot          = false
+#     enable_vtpm                 = true
+#   }
+
+#   labels = {
+#     "ec-src" = "vm_add-rest"
+#   }
+#   metadata = {
+#     ssh-keys = "${var.gce_ssh_user}:${file(var.gce_ssh_pub_key_file)}"
+#   }
+# }
+
+
+resource "google_compute_instance" "ii-free-tier" {
+  name         = "ii-free-tier"
+  machine_type = "e2-micro"
   zone         = var.zone
+
   boot_disk {
+    auto_delete = true
+    device_name = "free-tier"
+
     initialize_params {
-      image = "ubuntu-os-cloud/ubuntu-2004-focal-v20230302"
-      size  = "80"
-      type  = "pd-balanced"
+      image = "projects/ubuntu-os-cloud/global/images/ubuntu-2004-focal-v20230302"
+      size  = 30
+      type  = "pd-standard"
     }
+
+    mode = "READ_WRITE"
   }
 
   network_interface {
-    network = "default"
-    # subnetwork = "regions/us-west1/subnetworks/default"
-    subnetwork = "regions/${var.region}/subnetworks/default"
     access_config {
-      #   name         = "External NAT"
-      network_tier = "PREMIUM"
+      network_tier = "STANDARD"
     }
+    subnetwork = "projects/${var.project}/regions/${var.region}/subnetworks/default"
   }
 
+  can_ip_forward            = false
+  deletion_protection       = false
+  enable_display            = false
   allow_stopping_for_update = true
 
-  metadata = {
-    ssh-keys = "${var.gce_ssh_user}:${file(var.gce_ssh_pub_key_file)}"
+  labels = {
+    ec-src = "vm_add-tf"
   }
 
   tags = ["http-server", "https-server"]
 
-  # service_account {
-  #   email = "557177865159-compute@developer.gserviceaccount.com"
-  #   scopes = ["https://www.googleapis.com/auth/devstorage.read_only",
-  #     "https://www.googleapis.com/auth/logging.write",
-  #     "https://www.googleapis.com/auth/monitoring.write",
-  #     "https://www.googleapis.com/auth/servicecontrol",
-  #     "https://www.googleapis.com/auth/service.management.readonly",
-  #   "https://www.googleapis.com/auth/trace.append"]
-  # }
+
+
 
   scheduling {
     automatic_restart   = true
     on_host_maintenance = "MIGRATE"
+    preemptible         = false
     provisioning_model  = "STANDARD"
   }
+
 
   shielded_instance_config {
     enable_integrity_monitoring = true
@@ -99,12 +158,12 @@ resource "google_compute_instance" "deincidentcompute" {
     enable_vtpm                 = true
   }
 
-  deletion_protection = false
-  description         = ""
-  labels = {
-    "ec-src" = "vm_add-rest"
+  metadata = {
+    ssh-keys = "${var.gce_ssh_user}:${file(var.gce_ssh_pub_key_file)}"
   }
+
 }
+
 
 resource "google_storage_bucket" "data-lake-bucket" {
   location = var.region
